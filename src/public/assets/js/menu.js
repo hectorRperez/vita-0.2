03e9fea0fdcbe79694b3b1a31d1202bd8edb8eb1
gracrys <gracrys@gmail.com>
2021-06-16 20:32:47 -0400

    upated close functionality

--- src/public/assets/js/menu.js
+++ src/public/assets/js/menu.js
@@ -2,17 +2,19 @@ window.onload = () => {
 	let openFlag = false;
 	const menu =  document.querySelector(".menu")
 	document.querySelector(".hamburger").onclick = x => {
-		if(openFlag)
-		{
-			menu.classList.remove("opened")
-			openFlag = !openFlag
-			document.body.classList.remove("no-scroll")
-		}
-		else
+		
+		if(!openFlag)
 		{
 			menu.classList.add("opened")
 			openFlag = !openFlag
 			document.body.classList.add("no-scroll")
 		}
+	}
+	document.querySelector(".menu + .close").onclick = x =>{
+
+			menu.classList.remove("opened")
+			openFlag = !openFlag
+			document.body.classList.remove("no-scroll")
+
 	}
 }
