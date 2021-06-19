ddb24254fefb37e6a98c36d6175d83f7470c7db1
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-06-18 20:03:21 -0400

    retirada ruta inicial

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,5 @@
-const express = require('express');
+const router = require('express').Router();
 const passport = require('passport');
-const path = require('path');
-
-const router = express.Router();
-
-
-// ruta inicial
-router.get('/', (req, res) => {
-	res.render('index.ejs');
-});
-
 
 // ruta que muestra el formulario de login
 router.get('/login', (req, res) => {
