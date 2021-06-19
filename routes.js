3a77a8bd5b7a0056a9dceeed10e33e3d8d9ebf66
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-06-18 20:04:20 -0400

    nueva ruta agregada

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.use( require('./routes/auth.js') );
 router.use( require('./routes/shop.js') );
 router.use( require('./routes/products.js') );
+router.use( require('./routes/home.js') );
 
 module.exports = router;
 
