e2669a230297c10f2beacec653972bdd10db84d0
Hector Perez <hectotperez1238@gmail.com>
2021-06-21 23:11:23 -0400

    Nueva seccion de testimonio agregado

---
+++ public/assets/js/testimonials.js
@@ -0,0 +1,21 @@
+const slides = document.querySelector(".slider").children;
+const indicatorImages = document.querySelector(".slider-indicator").children;
+
+for (let index = 0; index < indicatorImages.length; index++) {
+    indicatorImages[index].addEventListener("click",function() {
+        // console.log(this.getAttribute("data-id"));
+
+        for (let j = 0; j < indicatorImages.length; j++) {
+            indicatorImages[j].classList.remove("active");
+        }
+
+        this.classList.add("active");
+        const id = this.getAttribute("data-id");
+
+        for (let j = 0; j < slides.length; j++) {
+            slides[j].classList.remove("active");
+        }
+
+        slides[id].classList.add("active");
+    });
+}
