5baf74b26b452088364b91758e20a7616b6e8c17
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-07-01 18:54:08 -0400

    ruta de la tienda

---
+++ routes/skincare.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+// ruta que se carga de traer los productos
+router.get('/skincare', (req, res) => {
+
+    res.render('skincare',);
+
+});
+
+
+
+
+module.exports = router;
