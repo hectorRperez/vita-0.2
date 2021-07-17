646ca03bd6ba578352e02536221edc2e9f55c406
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-07-17 01:08:05 -0400

    reajuste al solicitar una imagen

--- routes/products.js
+++ routes/products.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const queryBuilder = require("../config/queryBuilder.js");
+const querySync = require("../config/querySync.js");
 const connection = require('../config/connection');
-const { throws } = require('assert');
 
 
 // ruta que se carga de traer los productos
-router.get('/get_products', (req, res, next) => {
+router.get('/get_products/:page?', (req, res, next) => {
 	
 	if( req.isAuthenticated() ) return next();
 	
@@ -16,30 +16,36 @@ router.get('/get_products', (req, res, next) => {
 
 }, async (req, res) => {
 
+    const {body, params} = req;
+
     // obtengo los parametros
-    let params = req.body;
+    let data = body;
+
+    // la pagina a mostrar
+    data.page = req.params.page;
 
     // defino los selects
-    params.selects = [ 
+    data.selects = [ 
         {field: 'id', condition: 'products.id'},
         {field: 'name', condition: 'products.name'},
-        {field: 'image', condition: 'products.image'},
+        {field: 'image', condition: 'products_images.image'},
         {field: 'quantity', condition: 'products.quantity'},
         {field: 'price', condition: 'products.price'},
-        {field: 'category', condition: 'categories.name'},
         {field: 'assessment', condition: 'products.assessment'},
         {field: 'sales_quantity', condition: 'products.sales_quantity'}
     ];
 
-    // defino los joins
-    params.joins = [
-        {type: 'INNER', join: ['categories', 'products.category_id', '=', 'categories.id'] }
+    data.joins = [
+      {type: "INNER", join: ["products_images", 'products_images.product_id', '=', 'products.id'] }
     ];
 
     // realizo la consulta
-    const results = await queryBuilder('products', params);
+    const results = await queryBuilder('products', data);
 
-    res.render('products', {products: results, user: req.user});
+    let num_items = await querySync('select count(name) as quantity from products', null);
+    num_items = num_items[0].quantity / 20;
+
+    res.render('products', {products: results, pagination_items: num_items,  user: req.user});
 
 });
 
@@ -52,48 +58,48 @@ router.post('/create_product', (req, res, next) => {
 	res.redirect("/login");
 
 }, async (req, res) => {
-    try {
-      const {file, body} = req; 
+   
+	try {
+		const {files, body} = req; 
 
-      // validaciones de formulario ---------
+		console.log(files);
+		// validaciones de formulario ---------
 
-      if(body.name == "" )
-        throw "El nombre del producto es obligatorio";
+		if(body.name == "" )
+		throw "El nombre del producto es obligatorio";
 
-      if(body.price == "" )
-        throw "El precio del producto es obligatorio";
+		if(body.price == "" )
+		throw "El precio del producto es obligatorio";
 
-      if( isNaN(body.price) )
-      throw "El precio del producto no es correcto";
-      
-      if( isNaN(body.quantity) || body.quantity == "" )
-        throw "Ingrese una cantidad correcta";
-        
-      if( isNaN(body.category_id) )
-        throw "Ingrese una categoria correcta";
+		if( isNaN(body.price) )
+		throw "El precio del producto no es correcto";
+		
+		if( isNaN(body.quantity) || body.quantity == "" )
+		throw "Ingrese una cantidad correcta";
+		
+		if( isNaN(body.category_id) )
+		throw "Ingrese una categoria correcta";
 
-      if(body.category_id == "" )
-      throw "La categoria es obligatoria";
-      
-      let route_image = null
-      if(file != null && file != undefined){
-        route_image = `img/${file.filename}`;
-      }
-      //------------------------------------------
-      
+		if(body.category_id == "" )
+		throw "La categoria es obligatoria";
+		
+		//------------------------------------------
+		
 
-      // defino la consulta y los valores que se va a guardar
-      let sql = "INSERT INTO products (image, name, price, quantity, category_id) VALUES (?,?,?,?,?)";
-      let values = [ route_image, body.name, body.price, body.quantity, body.category_id]
+		// defino la consulta y los valores que se va a guardar
+		let sql = "INSERT INTO products (name, price, quantity, category_id) VALUES (?,?,?,?)";
+		const result = await querySync(sql, [body.name, body.price, body.quantity, body.category_id]).catch(error => {throw error});
 
-      // ejecuto la consulta
-      connection.query(sql,values, function (err, result) {
-        if (err) throw err;
-        console.log("agregado correctamente");
-      });
 
-      res.redirect('get_products');
-    
+		for (let i = 0; i < files.length; i++) {
+
+			let image = `img/${files[i].filename}`;
+			sql = "INSERT INTO products_images (product_id, image, is_first) VALUES (?,?,?)"
+			await querySync(sql, [result[0].insertId], image, i == 0).catch(error => {throw error});
+		}
+
+		res.redirect('get_products');
+
 
     } catch (error) {
 
