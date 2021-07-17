ed997babf66011b03c2beb1eefe106867d417834
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-07-17 00:14:57 -0400

    refectorizada la forma en que se consultan las img

--- routes/home.js
+++ routes/home.js
@@ -5,37 +5,37 @@ const router = require('express').Router();
 // ruta inicial
 router.get('/', async (req, res) => {
    
+
 	let params = {};
 
-	params.selects = [
-		{field: 'history', condition: 'home_info.history'},
-		{field: 'mision', condition: 'home_info.mision'},
-		{field: 'vision', condition: 'home_info.vision'}
-	];
-
-	let info = await queryBuilder('home_info', params);
-
-	let params2 = {};
-
 	// defino los selects
-	params2.selects = [
+	params.selects = [
 		{field: 'name', condition: 'products.name'},
-		{field: 'image', condition: 'products.image'},
+		{field: 'image', condition: 'products_images.image'},
 		{field: 'price', condition: 'products.price'},
 		{field: 'assessment', condition: 'products.assessment'},
 	];
 
     // defino los joins
-    params2.joins = [
-        {type: 'INNER', join: ['products', 'home_products.product_id', '=', 'products.id'] }
+    params.joins = [
+        {type: 'INNER', join: ['products', 'home_products.product_id', '=', 'products.id'] },
+		{type: "INNER", join: ["products_images", 'products_images.product_id', '=', 'products.id'] }
     ];
 
+	params.where = [
+		['products_images.is_first', '=', 1]
+	];
+
 	// establezco el limite de filas a mostrar
-	params2.limit = 4;
+	params.limit = 4;
 
-	let products = await queryBuilder('home_products', params2);
+	params
 
-	res.render('index.ejs', {info: info, products: products});
+	let products = await queryBuilder('home_products', params);
+
+	console.log(products);
+
+	res.render('index.ejs', {products: products});
 
 });
 
