be86ddd4b3d26f25020665138cc7a57d0aa7ba72
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-07-18 19:37:33 -0400

    implementadas las transacciones de mysql

--- config/querySync.js
+++ config/querySync.js
@@ -1,9 +1,10 @@
 const connection = require("./connection");
+const util = require('util');
 
 
 // funcion que me permite ejecutar consultas sql
 // y obtener los datos en una variable fuera de la funcion query
-module.exports = function (sql, values) {
+const querySync = function (sql, values) {
     return new Promise( function(resolve, reject) {
         connection.query(sql, values, (error, results, fields) => {
             if(error) return reject(error);
@@ -12,3 +13,23 @@ module.exports = function (sql, values) {
         });
     });
 }
+
+const beginTransaction= ()=> { 
+    return util.promisify(connection.beginTransaction).call(connection); 
+}
+
+const rollback= ()=> {
+    return util.promisify(connection.rollback).call(connection)
+}
+
+const commit= ()=> {
+    return util.promisify(connection.commit).call(connection)
+}
+
+
+module.exports = {
+    querySync: querySync,
+    beginTransaction: beginTransaction,
+    rollback: rollback,
+    commit: commit
+};
