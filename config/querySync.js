3a430a5d95ab895733c2c6925b74f14b5e639739
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-07-18 00:38:43 -0400

    comentarios agregados

--- config/querySync.js
+++ config/querySync.js
@@ -1,5 +1,8 @@
 const connection = require("./connection");
 
+
+// funcion que me permite ejecutar consultas sql
+// y obtener los datos en una variable fuera de la funcion query
 module.exports = function (sql, values) {
     return new Promise( function(resolve, reject) {
         connection.query(sql, values, (error, results, fields) => {
