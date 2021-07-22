339b2596bfa80185b7728b76871a1aecf3c61c16
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-07-22 18:58:33 -0400

    nueva vista de contacto

---
+++ routes/contact_us.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+// ruta inicial
+router.get('/contact', (req, res) => {
+   
+
+	try {
+		res.render('contact_us.ejs');
+	} catch (error) {
+		console.error(error);
+	}
+
+});
+
+module.exports = router;
