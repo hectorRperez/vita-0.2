376571865d7829ddcb1c2ec28972258f571e8e11
Hector Perez <hectotperez1238@gmail.com>
2021-08-31 14:50:54 -0400

    Added scrollrevealjs library

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -2,29 +2,22 @@
 //check how the views are hooked, and in which order
 //
 
-	let openFlag = false;
-	const menu =  document.querySelector(".menu")
-	console.log(document.querySelector(".hamburger"))
-	document.querySelector(".hamburger").onclick = x => {
-		
-		if(!openFlag)
-		{
-			menu.classList.add("opened")
-			openFlag = !openFlag
-			document.body.classList.add("no-scroll")
-		}
+let openFlag = false;
+const menu = document.querySelector('.menu');
+console.log(document.querySelector('.hamburger'));
+document.querySelector('.hamburger').onclick = (x) => {
+	if (!openFlag) {
+		menu.classList.add('opened');
+		openFlag = !openFlag;
+		document.body.classList.add('no-scroll');
 	}
-	document.querySelector(".menu + .close").onclick = x =>{
+};
+document.querySelector('.menu + .close').onclick = (x) => {
+	menu.classList.remove('opened');
+	openFlag = !openFlag;
+	document.body.classList.remove('no-scroll');
+};
 
-			menu.classList.remove("opened")
-			openFlag = !openFlag
-			document.body.classList.remove("no-scroll")
-
-	}
-
-	// if(document.querySelector(".menu.opened")){
-
-	// 	document.querySelector(".menu.opened .has-submenu").onclick = x => {
-	// 		x.preventDefault()
-	// 	}
-	// }
+console.log('scroll');
+// scrollreveal APLICADO AL HOME Y A SKINCARE
+ScrollReveal().reveal('.section-scroll', { delay: 400 });
