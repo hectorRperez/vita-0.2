30ea75acea66ba03d16378de9d23fcd5e50eb85c
Erik Mota <kenshimota@gmail.com>
2021-10-18 22:56:26 -0400

    add function render for views

--- routes/shop.js
+++ routes/shop.js
@@ -1,103 +1,115 @@
-'use strict'
+'use strict';
 
 const router = require('express').Router();
-const queryBuilder = require("../config/queryBuilder.js");
+const queryBuilder = require('../config/queryBuilder.js');
+const { RenderProduct } = require('../config/render-view');
 
 // ruta que se carga si el usuario ha iniciado sesion
-router.get('/shop:product_id?', (req, res, next) => {
+router.get(
+  '/shop:product_id?',
+  (req, res, next) => {
+    if (req.isAuthenticated()) return next();
 
+    // guardo la peticion en una cookie para depues continuar al estar autenticado
+    if (req.params.product_id != undefined || req.params.product_id != null) {
+      res.cookie('shop_product', req.params.product_id, {
+        expires: new Date(Date.now() + 30000),
+        httpOnly: true,
+      });
+    }
 
-	if( req.isAuthenticated() ) return next();
-	
-	// guardo la peticion en una cookie para depues continuar al estar autenticado
-	if(req.params.product_id != undefined || req.params.product_id != null) {
-		res.cookie('shop_product', req.params.product_id, { expires: new Date(Date.now() + 30000), httpOnly: true });
-	}
+    res.redirect('/login');
+  },
+  async (req, res) => {
+    try {
+      let product_id = null;
 
-	res.redirect("/login");
+      if (req.params.product_id) product_id = req.params.product_id;
+      else if (req.cookies.shop_product) product_id = req.cookies.shop_product;
 
-}, async (req, res) => {
+      let params = {};
 
-	try {
-		let product_id = null;
+      // defino los selects
+      params.selects = [
+        { field: 'id', condition: 'products.id' },
+        { field: 'name', condition: 'products.name' },
+        { field: 'price', condition: 'products.price' },
+        { field: 'image', condition: 'products_images.image' },
+        { field: 'description', condition: 'products.description' },
+        { field: 'category_id', condition: 'products.category_id' },
+      ];
 
-		if(req.params.product_id != undefined || req.params.product_id != null) {
-			product_id = req.params.product_id;
-		}else if(req.cookies.shop_product != undefined || req.cookies.shop_product != null){
-			product_id = req.cookies.shop_product;
-		}
+      params.joins = [
+        {
+          type: 'INNER',
+          join: ['products_images', 'products_images.product_id', '=', 'products.id'],
+        },
+      ];
 
+      params.where = [
+        ['products.id', '=', product_id],
+        ['products_images.is_first', '=', 1],
+      ];
 
-		let params = {};
+      let product = await queryBuilder('products', params)
+        .then(r => r[0])
+        .catch(err => {
+          throw err;
+        });
 
-		// defino los selects
-		params.selects = [
-			{field: 'id', condition: 'products.id'},
-			{field: 'name', condition: 'products.name'},
-			{field: 'price', condition: 'products.price'},
-			{field: 'image', condition: 'products_images.image'},
-			{field: 'description', condition: 'products.description'},
-			{field: 'category_id', condition: 'products.category_id'}
-		];
+      params = {};
 
-		params.joins = [
-			{type: "INNER", join: ["products_images", 'products_images.product_id', '=', 'products.id'] }
-		];
+      // defino los selects
+      params.selects = [
+        { field: 'image', condition: 'products_images.image' },
+        { field: 'is_first', condition: 'products_images.is_first' },
+      ];
 
-		params.where = [
-			['products.id', '=', product_id],
-			['products_images.is_first', '=', 1]
+      params.where = [['products_images.product_id', '=', product_id]];
 
-		];
+      let product_images = await queryBuilder('products_images', params).catch(err => {
+        throw err;
+      });
 
-		let product = await queryBuilder('products', params).then( r => r[0] ).catch( err => {throw err} );
+      params = {};
 
-		params = {};
+      // defino los selects
+      params.selects = [
+        { field: 'name', condition: 'products.name' },
+        { field: 'price', condition: 'products.price' },
+        { field: 'image', condition: 'products_images.image' },
+        { field: 'description', condition: 'products.description' },
+      ];
 
-		// defino los selects
-		params.selects = [
-			{field: 'image', condition: 'products_images.image'},
-			{field: 'is_first', condition: 'products_images.is_first'}
-		];
+      params.joins = [
+        {
+          type: 'INNER',
+          join: ['products_images', 'products_images.product_id', '=', 'products.id'],
+        },
+      ];
 
-		params.where = [
-			['products_images.product_id', '=', product_id]
-		];
+      params.where = [
+        ['products.category_id', '=', product.category_id],
+        ['products.id', '<>', product.id],
+        ['products_images.is_first', '=', 1],
+      ];
 
-		let product_images = await queryBuilder('products_images', params).catch( err => {throw err} );
+      let products_related = await queryBuilder('products', params).catch(err => {
+        throw err;
+      });
 
+      const view = RenderProduct(product);
 
-
-		params = {};
-
-		// defino los selects
-		params.selects = [
-			{field: 'name', condition: 'products.name'},
-			{field: 'price', condition: 'products.price'},
-			{field: 'image', condition: 'products_images.image'},
-			{field: 'description', condition: 'products.description'}
-		];
-
-		
-		params.joins = [
-			{type: "INNER", join: ["products_images", 'products_images.product_id', '=', 'products.id'] }
-		];
-
-		params.where = [
-			['products.category_id', '=', product.category_id],
-			['products.id', '<>', product.id],
-			['products_images.is_first', '=', 1]
-		];
-
-		let products_related = await queryBuilder('products', params).catch( err => {throw err} );
-
-		res.render('view_products', {user: req.user, product: product, product_images: product_images, products_related: products_related});
-	} catch (error) {
-		console.error(error);
-	}
-});
-
-
-
+      res.render(view, {
+        user: req.user,
+        product: product,
+        product_images: product_images,
+        products_related: products_related,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
 
 module.exports = router;
